576709fe9496770be22f296094a2f99b5e18089e
Mike Wojciechowski <mikew@sevenfifty.com>
2019-05-28 15:38:13 -0400

    Add script to update sqlite from a release download

---
+++ update_sqlite.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+here="$(dirname "$(realpath "$0")")"
+
+if [ ! -d "$1" ]; then
+  echo "Provide path to sqlite3 amalgamation"
+  exit 1
+fi
+
+version="$(awk '/^#define SQLITE_VERSION / { print $3 }' "$1/sqlite3.h")"
+
+cp "$1/sqlite3.h" "$here/ext/sqlite3/"
+cp "$1/sqlite3.c" "$here/ext/sqlite3/sqlite3_core.c"
+
+sed -Ei '' "s/^  s.version = \"[^\"]+\"$/  s.version = $version/;s/^  s.date = \"[^\"]+\"$/  s.date = \"$(date +%F)\"/" "$here/sqlite3-static.gemspec"
