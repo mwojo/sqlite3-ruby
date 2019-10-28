1eb31f188711d5dd9443fd45a9cdb54a7de47c38
Mike Wojciechowski <mikew@sevenfifty.com>
2019-10-28 10:47:36 -0400

    Include sqlite3ext.h in ext/ headers

--- update_sqlite.sh
+++ update_sqlite.sh
@@ -9,7 +9,7 @@ fi
 
 version="$(awk '/^#define SQLITE_VERSION / { print $3 }' "$1/sqlite3.h")"
 
-cp "$1/sqlite3.h" "$here/ext/sqlite3/"
+cp "$1/sqlite3.h" "$1/sqlite3ext.h" "$here/ext/sqlite3/"
 cp "$1/sqlite3.c" "$here/ext/sqlite3/sqlite3_core.c"
 
 sed -Ei '' "s/^  s.version = \"[^\"]+\"$/  s.version = $version/;s/^  s.date = \"[^\"]+\"$/  s.date = \"$(date +%F)\"/" "$here/sqlite3-static.gemspec"
